c4d0dd86c4cb66213df0b6f72c48e3941448992f
Khanh261 <giakhanh006@gmail.com>
2024-09-12 19:21:57 +0700

    add motion in landing page

--- app/(marketing)/_components/Advertise.tsx
+++ app/(marketing)/_components/Advertise.tsx
@@ -1,38 +1,63 @@
+"use client";
 import Button from "@/components/ui/Button";
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motion";
 
 const Advertise = () => {
   return (
-    <section className=" text-gray-700 body-font">
-      <div className=" container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-gray-900">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-            architecto?
-            <br className="hidden xl:inline-block" />
-            readymade gluten
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-            doloremque perspiciatis sequi fugiat dolorem necessitatibus eveniet
-            perferendis adipisci laborum numquam.
-          </p>
-          <div className="flex justify-center">
-            <Button>Button</Button>
-          </div>
+    <motion.div
+      initial={"hidden"}
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.25 }}
+    >
+      <section className=" text-gray-700 body-font">
+        <div className=" container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <motion.div
+            variants={fadeIn({
+              direction: "right",
+              type: "spring",
+              delay: 0.2,
+              duration: 1,
+            })}
+            className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+          >
+            <h1 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-gray-900">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              architecto?
+              <br className="hidden xl:inline-block" />
+              readymade gluten
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
+              doloremque perspiciatis sequi fugiat dolorem necessitatibus
+              eveniet perferendis adipisci laborum numquam.
+            </p>
+            <div className="flex justify-center">
+              <Button>Button</Button>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn({
+              direction: "left",
+              type: "spring",
+              delay: 0.5,
+              duration: 1,
+            })}
+            className="xl:max-w-lg lg:w-full md:w-1/2 w-5/6"
+          >
+            <Image
+              src="/images/calender.png"
+              alt="hero"
+              width={720}
+              height={600}
+              quality={100}
+            />
+          </motion.div>
         </div>
-        <div className="xl:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <Image
-            src="/images/calender.png"
-            alt="hero"
-            width={720}
-            height={600}
-            quality={100}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </motion.div>
   );
 };
 
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import FeatureBenefit from "./_components/FeatureBenefit";
 import Advertise from "./_components/Advertise";
-import Banner from "./_components/Banner";
 import Faq from "./_components/Faq";
+import Intro from "./_components/Intro";
 
 const MarketingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col">
-      <Banner />
-      <div className="flex items-center justify-center flex-col">
-        <div
-          className="mt-20 md:mt-100 mb-4 flex items-center border shadow-sm p-4
-        bg-blue-300 text-white rounded-full uppercase 
-        "
-        >
-          No 1 Schedule Management
-        </div>
-        <h1 className="text-3xl md:text-6xl text-center text-blue-800 mb-6">
-          Welcome to TIMEWISE
-        </h1>
-        <div
-          className="text-3xl md:text-6xl bg-gradient-to-r
-        from-blue-400 to bg-blue-600 text-white px-4 p-2 rounded-lg pb-4 w-fit"
-        >
-          Work Forward
-        </div>
-      </div>
+    <div className="flex items-center justify-center flex-col ">
+      <Intro />
       <FeatureBenefit />
       <Advertise />
       <Faq />
