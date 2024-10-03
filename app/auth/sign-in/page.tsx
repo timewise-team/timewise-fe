7dee31f4819b473bb8e641fbf122f376a791af15
Khanh261 <giakhanh006@gmail.com>
2024-10-04 01:02:44 +0700

    remove unused component

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,12 +1,7 @@
-import Login from "@/components/auth/login-form";
 import React from "react";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default LoginPage;
