397c868be0ad8a44df45636d2bc836e15d190563
anh.nguyenduc4 <anh.nguyenduc.work@gmail.com>
2024-12-02 09:47:17 +0700

    feature: update manage workspace screen (add delete and create workspace)

--- hooks/useLinkedEmailForManage.ts
+++ hooks/useLinkedEmailForManage.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import {useStateContext} from "@/stores/StateContext";
